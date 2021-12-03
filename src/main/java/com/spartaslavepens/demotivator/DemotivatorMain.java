291127b464b17de0c8460020976834ffc1616ef0
Natasha <natasha.pearl08@gmail.com>
2021-12-03 13:48:50 +0000

    updated DemotivatorMain.java

--- src/main/java/com/spartaslavepens/demotivator/DemotivatorMain.java
+++ src/main/java/com/spartaslavepens/demotivator/DemotivatorMain.java
@@ -1,6 +1,12 @@
 package com.spartaslavepens.demotivator;
 
 import com.spartaslavepens.CommandRunner;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 public class DemotivatorMain implements CommandRunner {
+
+    @Override
+    public void start(MessageReceivedEvent event) {
+
+    }
 }
