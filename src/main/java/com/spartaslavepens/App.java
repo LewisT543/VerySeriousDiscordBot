44f3b865d65e68470bb42bdebe1b432649cb2511
Lewis <lewisthornton@btinternet.com>
2021-12-03 17:05:05 +0000

    Fixed some stuff, can now tag anyone online or offline

--- src/main/java/com/spartaslavepens/App.java
+++ src/main/java/com/spartaslavepens/App.java
@@ -4,6 +4,9 @@ import com.spartaslavepens.factory.Clairvoyant;
 import com.spartaslavepens.utils.PropertyManager;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.ChunkingFilter;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
 
 import javax.security.auth.login.LoginException;
 
@@ -12,6 +15,9 @@ public class App {
     public static void main( String[] args ) {
         try {
             JDA jdaBuilder = JDABuilder.createDefault(PropertyManager.getToken())
+                    .setChunkingFilter(ChunkingFilter.ALL)
+                    .setMemberCachePolicy(MemberCachePolicy.ALL)
+                    .enableIntents(GatewayIntent.GUILD_MEMBERS)
                     .addEventListeners(new Clairvoyant())
                     .build();
         } catch (LoginException e) {
