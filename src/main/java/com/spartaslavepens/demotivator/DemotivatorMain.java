644c13d817995971cab7acd605122ba31be4a5bb
Lewis <lewisthornton@btinternet.com>
2021-12-03 13:48:07 +0000

    Half finished demotivator

--- src/main/java/com/spartaslavepens/demotivator/DemotivatorMain.java
+++ src/main/java/com/spartaslavepens/demotivator/DemotivatorMain.java
@@ -1,6 +1,23 @@
 package com.spartaslavepens.demotivator;
 
 import com.spartaslavepens.CommandRunner;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 public class DemotivatorMain implements CommandRunner {
+
+    @Override
+    public void start(MessageReceivedEvent event) {
+        sendNegativeResponse(event);
+    }
+
+    public void sendNegativeResponse(MessageReceivedEvent event) {
+        String name = event.getAuthor().getName();
+        String negMessage = getNegativeMessage();
+        event.getChannel().sendMessage("Negative Message").queue();
+    }
+
+    public String getNegativeMessage() {
+
+    }
 }
