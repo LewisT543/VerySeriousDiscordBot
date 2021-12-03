608918b5b8ad4895948d882ae03ef439f99f7aa8
Lewis <lewisthornton@btinternet.com>
2021-12-03 14:35:23 +0000

    minor changes

--- src/main/java/com/spartaslavepens/App.java
+++ src/main/java/com/spartaslavepens/App.java
@@ -3,9 +3,6 @@ package com.spartaslavepens;
 import com.spartaslavepens.utils.PropertyManager;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
-import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import org.jetbrains.annotations.NotNull;
 
 import javax.security.auth.login.LoginException;
 
